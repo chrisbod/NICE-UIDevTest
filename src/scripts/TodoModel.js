333fda9a4e6e209b621942903c6da87e6c9801af
Christian Bodart <chris@bodar.com>
2014-06-03 11:20:53 +0100

    Updating spec to check that ids cannot be changed

--- src/scripts/TodoModel.js
+++ src/scripts/TodoModel.js
@@ -1,9 +1,16 @@
 /*for the moment keeping our objects in the global namespace*/
-function TodoModel() {
+function TodoModel(text) {
     this._id = "todo" + TodoModel.count++;
+    if (text) {
+        this.text(text)
+    }
 }
 TodoModel.count = 0;
-TodoModel.prototype.id = function(value) {
+TodoModel.prototype.id = function() {
     return this._id;
 }
 TodoModel.prototype._id = "";
+TodoModel.prototype.text = function(text) {
+    return this._text = text;
+}
+TodoModel.prototype._text = "";
--- tests/spec/ToDoSpec.js
+++ tests/spec/ToDoSpec.js
@@ -15,12 +15,15 @@ describe("Todo Data Model tests", function() {
 
     //programmatical tests//
 
-    it("A blank todo should have a unique id",
+    it("A blank todo should have a readonly unique id",
         //can't help being a programmer so we'll expect every todo to have an id (but not check it's actually value as we don't know it's format)
         function() {
             expect(todo.id()).toBeTruthy();
             var newTodo = new TodoModel();
             expect(newTodo.id()).not.toEqual(todo.id())
+            var id = newTodo.id();
+            newTodo.id("testId");
+            expect(id).toEqual(newTodo.id())
         }
     );
     //More Behavioural states
