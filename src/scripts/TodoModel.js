15867829b44dbc77aad065673c45540e8ff7e1e5
Christian Bodart <chris@bodar.com>
2014-06-03 11:16:11 +0100

    Adding basic model members tests (failing)

--- src/scripts/TodoModel.js
+++ src/scripts/TodoModel.js
@@ -1,2 +1,9 @@
 /*for the moment keeping our objects in the global namespace*/
-function TodoModel() {}
+function TodoModel() {
+    this._id = "todo" + TodoModel.count++;
+}
+TodoModel.count = 0;
+TodoModel.prototype.id = function(value) {
+    return this._id;
+}
+TodoModel.prototype._id = "";
--- tests/spec/ToDoSpec.js
+++ tests/spec/ToDoSpec.js
@@ -13,13 +13,35 @@ describe("Todo Data Model tests", function() {
         }
     );
 
+    //programmatical tests//
+
     it("A blank todo should have a unique id",
         //can't help being a programmer so we'll expect every todo to have an id (but not check it's actually value as we don't know it's format)
         function() {
             expect(todo.id()).toBeTruthy();
             var newTodo = new TodoModel();
-            expect(newTodo.id()).toNotEqual(todo.id())
+            expect(newTodo.id()).not.toEqual(todo.id())
         }
     );
-
+    //More Behavioural states
+    it("A blank todo should be instantiated with an empty text field",
+        function() {
+            expect(todo.text()).toEqual("");
+        }
+    );
+    it("A blank todo should be instantiated with an incomplete state",
+        function() {
+            expect(todo.state()).toEqual("incomplete")
+        }
+    );
+    it("A blank todo should not be in an active state (in this case focussed or hovered over)",
+        function() {
+            expect(todo.active()).toEqual(false);
+        }
+    );
+    it("A blank todo should not be being edited",
+        function() {
+            expect(todo.editing()).toEqual(false);
+        }
+    );
 })
