c5a5cd5a391e093a620ce87c92ec373e8dd8cec4
Christian Bodart <chris@bodar.com>
2014-06-03 12:34:06 +0100

    passing more TodoManager tests

--- src/scripts/TodoManager.js
+++ src/scripts/TodoManager.js
@@ -1,2 +1,24 @@
-/*this is the view controller (or view model if we're using MVVM)*/
-function TodoManager() {}
+/*this is the view controller (or view model if we're using MVVM/knockout)*/
+function TodoManager() {
+    this._todos = [];
+    this.completedTodos = []
+}
+TodoManager.prototype = {
+    _todos: null, //array
+    todos: function() {
+        return this._todos;
+    },
+    _completedTodos: null, //array
+    completedTodos: function() {
+        return this._completedTodos
+    },
+    newTodo: function() {
+
+    },
+    removeTodo: function(todo) {
+
+    },
+    completeTodo: function(todo) {
+
+    }
+}
--- tests/spec/TodoManagerSpec.js
+++ tests/spec/TodoManagerSpec.js
@@ -1,16 +1,17 @@
 describe("TodoManager unit tests", function() {
+    //Note this should be using a helper that it doesn't really know the interface
     var todoManager;
     beforeEach(
         function() {
             todoManager = new TodoManager();
         }
     );
-    expect("new Todo should have zero todos",
+    it("new Todo should have zero todos",
         function() {
             expect(todoManager.todos().length).toEqual(0);
         }
     );
-    expect("adding a todo should create a todo, insert it into our todos",
+    it("adding a todo should create a todo, insert it into our todos",
         function() {
             var newTodo = todoManager.newTodo();
             //again working against a knockout style interface (and assuming our collections are arrays)
@@ -18,7 +19,7 @@ describe("TodoManager unit tests", function() {
             expect(todoManager.todos()[0]).toEqual(newTodo);
         }
     );
-    expect("removing a todo (not deleting) should give us a removed todo but not delete the todo",
+    it("removing a todo (not deleting) should give us a removed todo but not delete the todo",
         function() {
             var newTodo = todoManager.newTodo();
             todoManager.removeTodo(newTodo);
