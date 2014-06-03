7f881d06c447df1fa0103f4de7311d4ac37c6758
Christian Bodart <chris@bodar.com>
2014-06-03 11:26:53 +0100

    fulfilled basic model interface

--- src/scripts/TodoModel.js
+++ src/scripts/TodoModel.js
@@ -10,7 +10,31 @@ TodoModel.prototype.id = function() {
     return this._id;
 }
 TodoModel.prototype._id = "";
-TodoModel.prototype.text = function(text) {
-    return this._text = text;
+TodoModel.prototype.text = function() {
+    if (arguments.length) {
+        this._text = arguments[0];
+    }
+    return this._text;
 }
 TodoModel.prototype._text = "";
+TodoModel.prototype.state = function() {
+    if (arguments.length) {
+        this._state = arguments[0];
+    }
+    return this._state;
+}
+TodoModel.prototype._state = "incomplete";
+TodoModel.prototype.active = function() {
+    if (arguments.length) {
+        this._active = arguments[0];
+    }
+    return this._active;
+}
+TodoModel.prototype._active = false;
+TodoModel.prototype.editing = function() {
+    if (arguments.length) {
+        this._active = arguments[0];
+    }
+    return this._active;
+}
+TodoModel.prototype._editing = false;
