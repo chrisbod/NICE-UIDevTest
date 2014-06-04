12ddf6febc3229b65789f9c0fe39cee016997f20
Christian Bodart <chris@bodar.com>
2014-06-04 15:40:50 +0100

    knockout model passing all tests

--- src/scripts/knockout-TodoModel.js
+++ src/scripts/knockout-TodoModel.js
@@ -1,16 +1,18 @@
 function TodoModel(data) {
     this._id = "todo" + TodoModel.count++;
     this.text = ko.observable(data.text || this.text());
-    this.state = ko.observable(data.state || this.state());
+    this.state = ko.observable(data.state || this.state()).extend({
+        allowedValues: this.state_allowedValues
+    });
     this.active = ko.observable(data.active || this.active());
     this.editing = ko.observable(data.editing || this.editing());
 }
 TodoModel.count = 0;
-
-
 TodoModel.prototype.id = function() {
     return this._id;
 }
+TodoModel.prototype.state_allowedValues = ["incomplete", "removed", "completed"];
+
 //Defaults
 TodoModel.prototype.text = function() {
     return '';
@@ -24,3 +26,18 @@ TodoModel.prototype.active = function() {
 TodoModel.prototype.editing = function() {
     return false;
 }
+
+
+//knockout extender to prevent illegal values being set on an observable
+//old value will be preserved
+ko.extenders.allowedValues = function(target, option) {
+    var lastValue
+    target.subscribe(function(oldValue) {
+        lastValue = oldValue;
+    }, null, 'beforeChange');
+    target.subscribe(function(newValue) {
+        if (option.indexOf(newValue) == -1) {
+            target(lastValue, newValue);
+        }
+    })
+};
