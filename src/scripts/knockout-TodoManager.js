714a5b46ddde4722d2da9e0cc82356dddbe09c24
Christian Bodart <chris@bodar.com>
2014-06-04 16:26:18 +0100

    all tests passing (!!)

--- src/scripts/knockout-TodoManager.js
+++ src/scripts/knockout-TodoManager.js
@@ -2,12 +2,38 @@ function TodoManager(data) {
     if (!data) {
         data = {};
     }
+    var self = this;
     this.todos = ko.observableArray(data.todos || this.todos());
+    this.removedTodos = ko.computed(function() {
+        return ko.utils.arrayFilter(self.todos(), self.todoIsRemoved);
+    });
+    this.completedTodos = ko.computed(function() {
+        return ko.utils.arrayFilter(self.todos(), self.todoIsComplete);
+    });
 }
 
 
 //defaults
-
 TodoManager.prototype.todos = function() {
     return [];
+}
+TodoManager.prototype.removedTodos = function() {
+    return [];
+}
+TodoManager.prototype.newTodo = function() {
+    var todo = new TodoModel();
+    this.todos.push(todo);
+    return todo;
+}
+TodoManager.prototype.removeTodo = function(todo) {
+    todo.state("removed");
+}
+TodoManager.prototype.todoIsRemoved = function(todo) {
+    return todo.state() == "removed";
+}
+TodoManager.prototype.completeTodo = function(todo) {
+    todo.state("completed");
+}
+TodoManager.prototype.todoIsComplete = function(todo) {
+    return todo.state() == "completed";
 }
